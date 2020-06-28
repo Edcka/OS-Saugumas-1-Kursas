4c5486771e5ad7cdd8905e3726e79fff80665eb9
Edcka <edvardastamb@gmail.com>
2020-06-28 13:04:06 +0300

    Add files via upload

---
+++ OSLAB4.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace OSLAB4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = @"C:\Users\Dede\Desktop\";
+            Process[] processes = Process.GetProcesses();
+            StreamWriter writer = File.CreateText(path+"CurrentProcessList.txt");
+            string date = DateTime.Now.ToString("yyyy-MM-dd HH-mm");
+            StreamWriter writeCSV = File.CreateText(path + "TotalMemoryUsage" +date+ ".csv");
+
+            
+
+            foreach (Process theprocess in processes)
+            {
+                string processStr = theprocess.ProcessName.ToString();
+                int processIdStr = theprocess.Id;
+                string pilnasPav = processIdStr + processStr;
+                long ramUsage = (theprocess.PeakPagedMemorySize64);
+                if(theprocess.ProcessName == "Idle") { continue; }
+                writer.WriteLine(" ID: {1}  Process: {0} ", processStr, processIdStr);
+            }
+
+            Console.WriteLine("Procesai irasyti i faila...");
+            Console.WriteLine("Skaiciuojami Procesoriu uzemantys procesai...");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                if (processes[i].ProcessName == "Idle") { continue; }
+                for (int j = 0; i <= processes.Length; j++)
+                {
+                    if (processes[j].ProcessName == "Idle") { break; }
+                    if (processes[i].TotalProcessorTime < processes[j].TotalProcessorTime)
+                    {
+                        Process temp = processes[i];
+                        processes[i] = processes[j];
+                        processes[j] = temp;
+                    }
+                }
+                Console.WriteLine("Procesas irasomas i CSV faila...");
+                writeCSV.WriteLine(" ID: {1}  Process: {0}  Proc: {2}", processes[i].ProcessName, processes[i].Id,
+                    processes[i].TotalProcessorTime);
+            }
+            Console.WriteLine("Irasymas baigtas...");
+
+            writer.Close();
+            writeCSV.Close();
+
+            Process[] notepad = Process.GetProcessesByName("notepad");
+            
+            if(notepad.Length == 0)
+            {
+                Console.WriteLine("Notepad neveikia");
+            }
+            else { 
+                Console.WriteLine("ID: {0}, RAM usage: {1} MB,"
+                       , notepad[0].Id, notepad[0].WorkingSet64 / 1000000);
+            }
+            Console.WriteLine( "bye bye"  );
+        }
+    }
+}
---
+++ OSLAB5_Final.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OSLAB5_Final
+{
+
+    public class Functions
+    {
+
+        public void ComputerStatistics()
+        {
+            try
+            {
+                Console.WriteLine("Computer name: " + ComputerName());
+                Console.WriteLine("CPU Usage: " + CpuUsage());
+                Console.WriteLine("Disk Stats: " + DiskStats());
+                IDictionary<string, Int64> ramData = MemmoryStats();
+                Console.WriteLine("Total physical memmory: " + ramData["Total physical memmory"] + " MB");
+                Console.WriteLine("Memmory usage: " + ramData["Memmory usage"] + " MB");
+                Console.WriteLine(Get10AppEvents("Firefox"));
+            }
+            catch (ManagementException e )
+            {
+                Console.WriteLine("An error occurred while querying for WMI data: " + e.Message);
+            }
+        }
+        private string DiskStats()
+        {
+            ManagementObjectSearcher dd = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+            List<string> stat = new List<string>();
+            foreach (ManagementObject current in dd.Get())
+            {
+                stat.Add(current["Model"] + " Size: " + current["Size"]);
+            }
+            return string.Join("\n", stat);
+        }
+        private string CpuUsage()
+        {
+            ManagementObjectSearcher fullData = new ManagementObjectSearcher("root\\CIMV2",
+                "SELECT * FROM Win32_PerfFormattedData_PerfOS_Processor WHERE Name=\"_Total\"");
+            ManagementObjectCollection currentData = fullData.Get();
+            ManagementObject queryObj = currentData.Cast<ManagementObject>().First();
+
+            return Convert.ToString(queryObj["PercentIdleTime"]);
+
+        }
+        private string ComputerName()
+        {
+            ManagementObjectSearcher fullData = new ManagementObjectSearcher("root\\CIMV2",
+                "SELECT Name FROM Win32_ComputerSystem");
+            ManagementObjectCollection currentData = fullData.Get();
+            ManagementObject queryObj = currentData.Cast<ManagementObject>().First();
+            return Convert.ToString(queryObj["Name"]);
+        }
+        private IDictionary<string, Int64> MemmoryStats()
+        {
+            Int64 ramUsage = Process.GetCurrentProcess().WorkingSet64;
+            string query = "SELECT Capacity FROM Win32_PhysicalMemory";
+            IDictionary<string, Int64> ramData = new Dictionary<string, Int64>();
+            ManagementObjectSearcher fullData = new ManagementObjectSearcher(query);
+
+            Int64 cap = 0;
+            foreach (ManagementObject currentData in fullData.Get())
+            {
+                cap += Convert.ToInt64(currentData.Properties["Capacity"].Value);
+
+            }
+            cap = cap / (1024 * 1024);
+            ramUsage = ramUsage / 1024;
+            ramData.Add("Total physical memmory", cap);
+            ramData.Add("Memmory usage", ramUsage);
+            return ramData;
+        }
+        private IDictionary<string, EventLog> Las10LogEntries()
+        {
+            IDictionary<string, EventLog> eventionary = new Dictionary<string, EventLog>();
+            EventLog events = new EventLog("Application", System.Environment.MachineName);
+            foreach (EventLogEntry entry in events.Entries.Cast<EventLogEntry>().Reverse().Take(10))
+            {
+                Console.WriteLine(entry.Source);
+            }
+            Console.WriteLine("Geting logs");
+            EventLog[] eventLogLIst;
+            eventLogLIst = EventLog.GetEventLogs();
+            foreach (EventLog log in eventLogLIst)
+            {
+                eventionary.Add(log.Log, log);
+            }
+
+            Console.WriteLine("Number of logs on computer: " + eventLogLIst.Length);
+            int count = 0;
+            foreach (EventLog log in eventLogLIst)
+            {
+                Console.WriteLine("Log: " + log.Log);
+                try
+                {
+                    foreach (EventLogEntry _event in log.Entries)
+                    {
+                        if (count > 9){break;}
+                        Console.WriteLine(count + " " + _event.Message);
+                        count++;
+                    }
+                    count = 0;
+                }
+                catch
+                {
+                    Console.WriteLine("Unable to open " + log.Log + " log");
+                }
+            }
+            return eventionary;
+        }
+
+        public string Get10AppEvents(string appName)
+        {
+            EventLog events = new EventLog("Application", Environment.MachineName);
+            return string.Join("\n", events.Entries.Cast<EventLogEntry>().Where(pv => pv.Source.Contains(appName)).Take(10).Select(pv => pv.Message));
+        }
+
+    }
+}
